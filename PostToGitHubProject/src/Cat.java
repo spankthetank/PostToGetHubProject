1246628a75ed5ce7ddce6c860fd12195a5f355c9
Spankthetank <hhogan8@gmail.com>
2015-02-10 22:08:14 -0500

    Two classes, one dog and Professor Roy's favorite: CATS

---
+++ PostToGitHubProject/src/Cat.java
@@ -0,0 +1,9 @@
+
+public class Cat {
+
+	public static void main(String[] args) {
+		System.out.println("I am a cat that meows, pures, and sleeps alot");
+
+	}
+
+}
---
+++ PostToGitHubProject/src/Dog.java
@@ -0,0 +1,9 @@
+
+public class Dog {
+
+	public static void main(String[] args) {
+		System.out.println("I am a dog that bites, barks, and eats people if your mean to me");
+
+	}
+
+}
